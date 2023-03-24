231fc9c0de190a13574e094ba53a0c05ec2ff9e2
Daniel Irvine <daniel@danielirvine.com>
2023-03-24 16:04:22 +0000

    Initial commit

---
+++ AuthWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AuthWebApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
---
+++ AuthWebApp/Program.cs
@@ -0,0 +1,72 @@
+﻿using System.Net;
+using AuthWebApp;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
+using Microsoft.IdentityModel.Logging;
+
+//IdentityModelEventSource.ShowPII = true;
+//ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+// DTI Added here
+builder.Services
+    .AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApp(builder.Configuration, "AzureAdB2C")
+        .EnableTokenAcquisitionToCallDownstreamApi()
+                .AddInMemoryTokenCaches();
+
+builder.Services.AddRazorPages()
+// DTI TODO: adding the next bit will validate tokens
+/*.AddMvcOptions(options =>
+{
+    var policy = new AuthorizationPolicyBuilder()
+                     .RequireAuthenticatedUser()
+                     .Build();
+    options.Filters.Add(new AuthorizeFilter(policy));
+})*/
+.AddMicrosoftIdentityUI();
+
+// DTI Added above
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+// DTI Added here
+app.UseAuthorization();
+app.UseAuthentication();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapRazorPages();
+    endpoints.MapControllers(); // Needs to be added to add the various controllers proviided by Web.UI
+});
+// DTI Added above
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
